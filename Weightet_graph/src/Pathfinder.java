b92d40fa7e5a7717dcd073fefe9349ed7014a8f3
Manuel Reich <errnesto@me.com>
2013-01-07 21:30:15 +0100

    started with cheapest path finder

--- Weightet_graph/src/Pathfinder.java
+++ Weightet_graph/src/Pathfinder.java
@@ -41,6 +41,51 @@ public class Pathfinder {
 		return -1;
 	}
 	
+	public int printCheapestPath(int numberOfVertecies, int numberOfEdges){
+		WeightetGraph graph = new WeightetGraph(numberOfVertecies,numberOfEdges);
+		Vertex[] vertecies = graph.getallVertecies(); 
+		//choose two random vertecies
+		int start = random.nextInt(numberOfVertecies);
+		int end = random.nextInt(numberOfVertecies);
+//			       for each vertex v in Graph:                     
+//			           dist[v] := infinity ;  
+//        			   previous[v] := undefined ;                            
+//    			   end for  
+		int[] dist = new int[numberOfVertecies];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		int[] prev = new int[numberOfVertecies];
+		Arrays.fill(prev, Integer.MAX_VALUE);		       
+//			       dist[source] := 0 ;                                        
+		dist[start] = 0;
+//			       Q := the set of all nodes in Graph;
+		LinkedList<Vertex> queue = new LinkedList<Vertex>();
+		for(int i = 0;i<vertecies.length;i++){
+			queue.add(vertecies[i]);
+		}
+//			      while Q is not empty:
+		while (!queue.isEmpty()){
+//			          u := vertex in Q with smallest distance in dist[] ; 
+//			          remove u from Q ;
+//			          if dist[u] = infinity:
+//			              break ;                                            // all remaining vertices are
+//			          end if                                                 // inaccessible from source
+//			          
+//			          for each neighbor v of u:                              // where v has not yet been 
+//			                                                                 // removed from Q.
+//			              alt := dist[u] + dist_between(u, v) ;
+//			              if alt < dist[v]:                                  // Relax (u,v,a)
+//			                  dist[v] := alt ;
+//			                  previous[v] := u ;
+//			                  decrease-key v in Q;                           // Reorder v in the Queue
+//			              end if
+//			          end for
+//			      end while
+		}
+//			  return dist;
+		return 0;
+
+	}
+	
 	/**
 	 * @param args
 	 */
