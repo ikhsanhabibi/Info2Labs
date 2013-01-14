27bce35637c864b865e858bce7a5e15ca404df52
Manuel Reich <errnesto@me.com>
2013-01-14 18:55:15 +0100

    added scrabble cheater function and isPermutation function

--- Scrabble/src/HashBook.java
+++ Scrabble/src/HashBook.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.ArrayList;
-
+import java.util.Iterator;
 
 public class HashBook {
 	private int modolo = 0;
@@ -9,12 +9,33 @@ public class HashBook {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		HashBook bla = new HashBook();
-		bla.hashWords();
+		HashBook hashBook = new HashBook();
+		hashBook.hashWords();
+		
+		String testword = "adc";
+		hashBook.scrabbleCheater(testword);
+		
 	}
+	/**
+	 * looks up the all permutions (plus collisions) of a word in a hash table
+	 * @param word the word or set of letters we want to get the permutations from
+	 * @return an Array list of all the permutaions (plus Collisions)
+	 */
 	public ArrayList<String> lookup(String word){
-		int pos = calculatePosition(word);
+		double hashCode = getHashCode(word);
+		int pos = (int) (hashCode%modolo);//get the position in the hash table
 		return hashBook[pos];
+	}
+	public void scrabbleCheater(String letters){
+		System.out.print("Words you can built from the following letters: "+letters+"\n\n");
+		ArrayList<String> permutaions = lookup(letters);
+		Iterator<String> it = permutaions.iterator();
+		while(it.hasNext()){
+			String out = it.next(); 
+			if(isPermutation(out,letters)){
+				System.out.println(out);
+			}
+		}
 	}
 	/**
 	 * reads in a list of words and hashes them in the way that anagrammes are mapped on the same hash
@@ -30,7 +51,8 @@ public class HashBook {
 		hashBook = ((ArrayList<String>[])new ArrayList[modolo]);
 		
 		for(int i=0;i<numberOfWords;i++){
-			int pos = calculatePosition(book[i]);
+			double hashCode = getHashCode(book[i]);
+			int pos = (int) (hashCode%modolo);//get the position in the hash table
 			if(hashBook[pos] == null){
 				hashBook[pos] = new ArrayList<String>();
 			}
@@ -39,24 +61,16 @@ public class HashBook {
 		return hashBook;
 	}
 	/**
-	 * calculates the postion in the hashTable
-	 * @param word the word to hash
-	 * @return the postition in the hash table
-	 */
-	private int calculatePosition(String word){
-		//normalize word and save it as array so we can sort it
-		word = word.toLowerCase();
-		String[] tempWord = word.split("(?!^)");
-		Arrays.sort(tempWord);//by sorting inside the word anagrams are mapped to the same number
-		double hashCode = getHashCode(tempWord);
-		return (int) (hashCode%modolo);//get the position in the hash table
-	}
-	/**
 	 * claculates a unique number that represents a string 
 	 * @param st the string given as a single letter string array
 	 * @return the hashNumber
 	 */
-	private double getHashCode(String[] st){
+	private double getHashCode(String word){
+		//normalize word and save it as array so we can sort it
+		word = word.toLowerCase();
+		String[] st = word.split("(?!^)");
+		Arrays.sort(st);//by sorting inside the word anagrams are mapped to the same number
+				
 		int length = st.length;
 		double hashCode = 0;
 		for(int i=0;i<length;i++){
@@ -64,6 +78,11 @@ public class HashBook {
 			hashCode += Math.pow(ch*31, length-i+1);
 		}
 		return hashCode;
+	}
+	private boolean isPermutation(String word,String compare){
+		double code1 = getHashCode(word);
+		double code2 = getHashCode(compare);
+		return (code1 == code2);
 	}
 	/**
 	 * gets the next higher prime number from a given number !a bit risky since we dont know when the next prime number will occour
