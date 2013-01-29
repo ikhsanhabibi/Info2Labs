fe705cbb7f95e4a6e4b0566a6371e29cfdbd7924
Manuel Reich <errnesto@me.com>
2013-01-29 23:07:20 +0100

    added some comments tried to improve gitignore

--- HistrgrammDejavu/HistrgrammDejavu/main.cpp
+++ HistrgrammDejavu/HistrgrammDejavu/main.cpp
@@ -12,10 +12,10 @@
 using namespace std;
 struct Histogram{
     int size;
-    int *chars;
+    int *chars; // we use a pointer because we dont want to specify the size of the array yet
 };
 
-
+// reads in a specified file and returns a frequency tabe of the letters of the alphabet inside
 Histogram anylyseFile(string filename){
     //the histogram
     Histogram h;
@@ -41,9 +41,8 @@ Histogram anylyseFile(string filename){
                 //save letters as number so that A -> 0 and Z -> 26;
             }else if(letter >= 'A' && letter <= 'Z'){
                 letter -= 'A';
-            }
-            //increase the position of the letter in the histogram
-            h.chars[letter] ++;
+            }//all charaters that does not fit in here are ignored
+            h.chars[letter] ++;//save the letter to the frequency table
         }
         myfile.close();
     }
@@ -53,6 +52,8 @@ Histogram anylyseFile(string filename){
     return h;
 }
 
+//writes a given frequency table to a .txt file
+//the formating is passed in as a parameter v for vertical and h for horizontal
 void writeFile(Histogram h,char format){
     string filename = "frequency";
     string filetype = ".txt";
@@ -79,11 +80,11 @@ void writeFile(Histogram h,char format){
         bool lastLine = false;
         //print a new line until an empty one was printed
         for(int t=0;!lastLine;t++){
-            lastLine = true;//expext every line to be the last one
+            lastLine = true;//expect every line to be empty at the begining
             for (int i=0; i<h.size; i++) {
                 if(t < h.chars[i]){
                     out << "*\t";
-                    lastLine = false;//this isa not an empty line
+                    lastLine = false;//this is not an empty line
                 }else{
                     out << "\t";
                 }
