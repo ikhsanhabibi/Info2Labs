f17301ac02ee7cee3b9c9009ea7bbb2c0f753480
Manuel Reich <errnesto@me.com>
2013-01-29 22:55:05 +0100

    gitignore updated for Xcode

---
+++ HistrgrammDejavu/HistrgrammDejavu/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HistrgrammDejavu
+//
+//  Created by Manuel Reich on 29.01.13.
+//  Copyright (c) 2013 Manuel Reich / Kay Shoppe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
